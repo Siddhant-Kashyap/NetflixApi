0f1e8112aeba891fa48a1dcae185300478915e82
Siddhant Kashyap <86836609+Siddhant-Kashyap@users.noreply.github.com>
2023-09-09 17:18:35 +0530

    SubscriptionModel

--- NetflixApi/Model/LocalUsers.cs
+++ NetflixApi/Model/LocalUsers.cs
@@ -8,7 +8,7 @@ namespace NetflixApi.Model
 
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
+        public ObjectId Id { get; set; }
         public string Name { get; set; }
         public string UserName { get; set; }
        
